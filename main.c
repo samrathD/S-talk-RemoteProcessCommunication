73fde0346fc999a9e058e43e94284c0385409914
sda160 <sda160@asb9700u-g07.csil.sfu.ca>
2024-02-12 21:15:04 -0800

    create blueprint

--- main.c
+++ main.c
@@ -2,18 +2,59 @@
 
 //First create a function that creates a socket
 
-//Create a function that that takes the keyboard argument
+//Create a function that that takes the keyboard argument - Keyboard
 
-//Create a function that send the data over to the other socket
+//Create a function that send the data over to the other socket - Send
 
 //Create a function that prints the data on the screen
 
 
+
+//*******************
 int main(){
 //Create 2 lists 
 //      - List 1 stores the argument from keyboard and send it over to the other socket
-    printf("Hello world!\n");
-    printf("Updating main\n");
-    printf("add 1\n");
 //      - List 2 stores recieves the data and prints it on the screen
+
+// Create 2 mutex 
+//       - One for list 1
+//       - One for list 2
+
+//Initialize sockets
+//       - One for list 1
+//       - One for list 2
+
+
+//Keyboard 
+//      - put this in a pthread
+//      - busy wait while loop always waiting for user response
+//      - gets the user response and strncpy response into a malloc string
+//      - append it in the list-1 
+
+//
+
+//Send
+//      - put this in a pthread
+//      - Arguments host's name and port
+//      - busy waiting to send a response(if the list is empty then pthread_wait())
+//      - if not empty remove message form list1 and send over the network to the server
+//      - Deallocate message
+
+//Recieve
+//      - put this in a pthread
+//      - Arguments recieving port and the list2
+//      - Busy wait while loop constantly waiting to recieve the message
+//      - Add the message in list 2 
+
+//Print_screen
+//      - put this in a pthread
+//      - Arguments list2
+//      - Busy wait while loop constantly waiting to print output
+//      - If list 2 is empty pthread_wait()
+//      - Remove message from the list and print to the screen
+
+
+//Shutdown and free list
+//      - shutdown all the threads and sockets
+//      - free the lists
 }
