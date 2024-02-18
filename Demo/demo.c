d5de592633bfdac15e257e624417ebf451dde3dd
sda160 <sda160@asb9838nu-e08.csil.sfu.ca>
2024-02-18 15:47:40 -0800

    update sendprocess

--- Demo/demo.c
+++ Demo/demo.c
@@ -3,7 +3,7 @@
 #include <netdb.h>
 #include <string.h>
 #include <unistd.h>
-
+#include "list.h"
 #define MSG_MAX_LEN 1024
 #define PORT 22110
 
@@ -27,30 +27,61 @@ int main(int argCount, char** args){
     char messageRx[MSG_MAX_LEN];
     //Recieving the data
     while(1 && strcmp(messageRx,"!\n")!=0){
+        // struct sockaddr_in sinRemote;
+        // unsigned int sin_len = sizeof(sinRemote);
+        // // char messageRx[MSG_MAX_LEN];
+        
+        // int bytesRx = recvfrom(socketDescriptor,messageRx,
+        // MSG_MAX_LEN,0,(struct sockaddr*)&sinRemote, &sin_len);
+
+        // //Null terminated(string)
+        // // int terminatedIdx = (bytesRx< MSG_MAX_LEN)? bytesRx:MSG_MAX_LEN - 1;
+
+        // // messageRx[terminatedIdx] = 0;
+        // printf("Message Recieved(%d bytes): '%s'",bytesRx,messageRx);
+
+        // //Extract the value form the messsage:
+        // int incMe = atoi(messageRx);
+
+        // //Create Reply
+        // char messageTx[MSG_MAX_LEN];
+        // sprintf(messageTx,"Math: %d + 1 = %d\n", incMe, incMe+1);
+
+        // //Send Reply
+        // sin_len = sizeof(sinRemote);
+        // sendto(socketDescriptor,messageTx,strlen(messageTx),0,(struct sockaddr*)&sinRemote,sin_len);
+        
+        List* receiveList = List_create();
+        // static pthread_mutex_t receiveMutex = INITIALIZE;
+
+        static int my_port;
+
+        //pthread_cond_t *printCondition;
+        
         struct sockaddr_in sinRemote;
         unsigned int sin_len = sizeof(sinRemote);
-        // char messageRx[MSG_MAX_LEN];
+        char messageRx[MSG_MAX_LEN];
+        memset(messageRx, 0, sizeof(messageRx));
         
-        int bytesRx = recvfrom(socketDescriptor,messageRx,
-        MSG_MAX_LEN,0,(struct sockaddr*)&sinRemote, &sin_len);
 
+        int bytesRx = recvfrom(socketDescriptor, messageRx, MSG_MAX_LEN, 0, 
+                                (struct sockaddr*) &sinRemote, &sin_len);
+        printf("Hello\n");                        
         //Null terminated(string)
-        // int terminatedIdx = (bytesRx< MSG_MAX_LEN)? bytesRx:MSG_MAX_LEN - 1;
+        int terminatedIdx = (bytesRx< MSG_MAX_LEN)? bytesRx:MSG_MAX_LEN - 1;
+        messageRx[terminatedIdx] = 0;
+       
+        //Lock Mutex
+       // pthread_mutex_lock(receiveMutex);
+        {
+            printf("Message recieved\n");
+            List_append(receiveList,messageRx);
+           // pthread_cond_signal(printCondition);
+        }
+       // pthread_mutex_unlock(receiveMutex);
 
-        // messageRx[terminatedIdx] = 0;
-        printf("Message Recieved(%d bytes): '%s'",bytesRx,messageRx);
-
-        //Extract the value form the messsage:
-        int incMe = atoi(messageRx);
-
-        //Create Reply
-        char messageTx[MSG_MAX_LEN];
-        sprintf(messageTx,"Math: %d + 1 = %d\n", incMe, incMe+1);
-
-        //Send Reply
-        sin_len = sizeof(sinRemote);
-        sendto(socketDescriptor,messageTx,strlen(messageTx),0,(struct sockaddr*)&sinRemote,sin_len);
+        printf("message received: %s\n", messageRx);
 
     }
-    close(socketDescriptor);
+   // close(socketDescriptor);
 }
